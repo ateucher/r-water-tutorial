54a03dde060ace0e50084082fe7e91a4a97b2b78
Andy Teucher <andy.teucher@gov.bc.ca>
2017-12-05 14:19:32 -0800

    Add summary of mtcars

--- cars.R
+++ cars.R
@@ -4,3 +4,11 @@ library(ggplot2)
 data("mtcars")
 
 glimpse(mtcars)
+
+mt_cars_summary <- mtcars %>% 
+  group_by(cyl) %>% 
+  summarise(mean_disp = mean(disp), 
+            mean_hp = mean(hp))
+
+mt_cars_summary
+
